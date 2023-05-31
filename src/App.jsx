7bb82e021786d7371b35a3fd58cc8979fd0f5fd8
nacho <nachomrk2@hotmail.com>
2023-05-31 17:53:33 -0300

    subiendo archivo vacio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <>
+      <Card/>
+    </>
+  )
+}
+
+export default App
